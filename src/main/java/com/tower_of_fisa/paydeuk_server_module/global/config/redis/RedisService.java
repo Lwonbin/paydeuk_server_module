88a277d5a01cc4ee18aa4ea7c69c926bb03755fd
eratchacha <papa021326@naver.com>
2025-06-05 16:57:52 +0900

    feat(redis): 레디스 기능 추가 [#14] 값 증가, ttl 설정 기능 추가, RedisController 삭제

--- src/main/java/com/tower_of_fisa/paydeuk_server_module/global/config/redis/RedisService.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_module/global/config/redis/RedisService.java
@@ -4,23 +4,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 public class RedisService {
 
-  private final RedisTemplate<String, Object> redisTemplate;
+  private final RedisTemplate<String, String> redisTemplate;
 
-  public void saveValue(String key, String value) {
-    redisTemplate.opsForValue().set(key, value);
+  // 키-값 저장 (TTL을 지정할 수 있는 메서드 추가)
+  public void saveValue(String key, String value, long ttl) {
+    redisTemplate.opsForValue().set(key, value, ttl, TimeUnit.SECONDS);
   }
 
   public String getValue(String key) {
-    return (String) redisTemplate.opsForValue().get(key);
+    String value = redisTemplate.opsForValue().get(key);
+    return value != null ? value : ""; // null 대신 빈 문자열을 반환
   }
 
-  public void updateValue(String key, String value) { redisTemplate.opsForValue().set(key, value); }
+  // Redis에서 해당 키의 값을 증가시키는 메서드
+  public void addValue(String key, Double increment) {
+    redisTemplate.opsForValue().increment(key, increment);
+  }
 
-  public void deleteKey(String key) {
-    redisTemplate.delete(key);
+  public long getRemainingSecondsUntilNextTwoMonthsFirstDay() {
+    Calendar now = Calendar.getInstance();
+    Calendar firstOfNextMonth = (Calendar) now.clone();
+    firstOfNextMonth.add(Calendar.MONTH, 2); // 다음 달로 이동
+    firstOfNextMonth.set(Calendar.DAY_OF_MONTH, 1); // 1일로 설정
+    firstOfNextMonth.set(Calendar.HOUR_OF_DAY, 0);
+    firstOfNextMonth.set(Calendar.MINUTE, 0);
+    firstOfNextMonth.set(Calendar.SECOND, 0);
+    firstOfNextMonth.set(Calendar.MILLISECOND, 0);
+
+    long diffInMillis = firstOfNextMonth.getTimeInMillis() - now.getTimeInMillis();
+    return TimeUnit.MILLISECONDS.toSeconds(diffInMillis);
   }
 }
